f0f0f3f1d1ab2b1cd62dd304d0d4d89826371031
CFSanchezV <u201720260@upc.edu.pe>
2020-11-17 17:02:07 -0500

    imgs, algorithm + UI

--- interfaz.py
+++ interfaz.py
@@ -3,76 +3,86 @@ import tkinter.font as tkFont
 from tkinter import ttk
 from tkinter import messagebox
 from PIL import ImageTk, Image
-
+from mainGUI import *
 import random as rand
+
 
 def actualizarEntradas(entradas):
     for i in range(len(entradas)):
         entradas[i] = entradas[i].get()
     return entradas
 
+
 def validarEntradasVacias(entradas):
-    if entradas != None:
+    if entradas is not None:
         for entrada in entradas:
             if len(entrada) == 0:
-                tk.messagebox.showwarning(title="Penguin Classification",message="Faltan completar algunas entradas.")
+                tk.messagebox.showwarning(
+                    title="Penguin Classification", message="Faltan completar algunas entradas.")
                 return None
         return entradas
 
-def valiarEntradasNumericas(entradas):
-    if entradas != None:
-        for i in range(1,5):
-            try:
-                entradas[i] = float(entradas[i])
-            except:
-                tk.messagebox.showerror(title="Penguin Classification",message="Entrada numérica no válida: %s"%entradas[i])
-                return None
-        return entradas
 
-def obtenerEspecie(inputs,outputs):
+# def validarEntradasNumericas(entradas):
+#     if entradas is not None:
+#         for i in range(1, 5):
+#             try:
+#                 entradas[i] = float(entradas[i])
+#             except:
+#                 tk.messagebox.showerror(
+#                     title="Penguin Classification", message="Entrada numérica no válida: %s" % entradas[i])
+#                 return None
+#         return entradas
+
+
+def obtenerEspecie(inputs, outputs):
     entradas = inputs[:]
     entradas = actualizarEntradas(entradas)
     entradas = validarEntradasVacias(entradas)
-    entradas = valiarEntradasNumericas(entradas)
+    # entradas = validarEntradasNumericas(entradas)
     salidas = outputs[:]
-    if entradas != None:
+    if entradas is not None:
         # *
         # * EJECUTAR ALGORITMO, LOS VALORES DE ENTRADA YA SE ENCUENTRAN VALIDADOS
         # *
+        print("Entradas:", end="")
         print(entradas)
+        especie = process_all_input(entradas)
+        # print(entradas)
         # *
         # * PROBANDO CON RESULTADO DE ESPECIE ADELIE
         # *
-        especie = rand.randint(1,3)
+        # especie = rand.randint(1, 3)
         if especie == 1:
-            image = Image.open("p_adelie.png")
-            image = image.resize((225,225),Image.ANTIALIAS)
+            image = Image.open("./imgs/p_adelie.png")
+            image = image.resize((225, 225), Image.ANTIALIAS)
             imageTk = ImageTk.PhotoImage(image)
             salidas[0].configure(image=imageTk)
             salidas[0].image = imageTk
             salidas[1]["text"] = "La especie del pingüino es Adelie"
         if especie == 2:
-            image = Image.open("p_chinstrap.png")
-            image = image.resize((225,225),Image.ANTIALIAS)
+            image = Image.open("./imgs/p_chinstrap.png")
+            image = image.resize((225, 225), Image.ANTIALIAS)
             imageTk = ImageTk.PhotoImage(image)
             salidas[0].configure(image=imageTk)
             salidas[0].image = imageTk
             salidas[1]["text"] = "La especie del pingüino es Chinstrap"
         if especie == 3:
-            image = Image.open("p_gentoo.png")
-            image = image.resize((225,225),Image.ANTIALIAS)
+            image = Image.open("./imgs/p_gentoo.png")
+            image = image.resize((225, 225), Image.ANTIALIAS)
             imageTk = ImageTk.PhotoImage(image)
             salidas[0].configure(image=imageTk)
             salidas[0].image = imageTk
             salidas[1]["text"] = "La especie del pingüino es Gentoo"
 
+
 def run():
     root = tk.Tk()
-    #root.geometry("300x300")
-    root.title("Penguin Classification")
+    # root.geometry("300x300")
+    root.title("Clasificación de Pinguinos")
     root.configure(bg="white")
-    root.resizable(width=True,height=True)
-    root.iconbitmap('icono.ico')
+    root.resizable(width=True, height=True)
+    root.iconbitmap('./imgs/icono.ico')
 
     content = tk.StringVar()
 
@@ -80,54 +90,70 @@ def run():
     fontStyle = tkFont.Font(family="Candara", size=12)
     secondaryFontStyle = tkFont.Font(family="Calibri Light", size=12)
     thirdFontStyle = tkFont.Font(family="Candara", size=12)
-    tk.Label(root,text="Hábitat",bg="white",font=fontStyle).grid(row=0,sticky='W')
-    tk.Label(root,text="Longitud del Pico",bg="white",font=fontStyle).grid(row=1,sticky='W')
-    tk.Label(root,text="Profundidad del Pico",bg="white",font=fontStyle).grid(row=2,sticky='W')
-    tk.Label(root,text="Longitud de la Aleta",bg="white",font=fontStyle).grid(row=3,sticky='W')
-    tk.Label(root,text="Masa Corporal",bg="white",font=fontStyle).grid(row=4,sticky='W')
-    tk.Label(root,text="Sexo",bg="white",font=fontStyle).grid(row=5,sticky='W')
+    tk.Label(root, text="Hábitat", bg="white",
+             font=fontStyle).grid(row=0, sticky='W')
+    tk.Label(root, text="Longitud del Pico", bg="white",
+             font=fontStyle).grid(row=1, sticky='W')
+    tk.Label(root, text="Profundidad del Pico", bg="white",
+             font=fontStyle).grid(row=2, sticky='W')
+    tk.Label(root, text="Longitud de la Aleta", bg="white",
+             font=fontStyle).grid(row=3, sticky='W')
+    tk.Label(root, text="Masa Corporal", bg="white",
+             font=fontStyle).grid(row=4, sticky='W')
+    tk.Label(root, text="Sexo", bg="white",
+             font=fontStyle).grid(row=5, sticky='W')
 
-    island = ttk.Combobox(root,font=secondaryFontStyle)
-    island["values"] = ["Torgersen","Biscoe","Dream"]
-    island.grid(row=0,column=1,sticky='E')
+    island = ttk.Combobox(root, font=secondaryFontStyle)
+    island["values"] = ["Torgersen", "Biscoe", "Dream"]
+    island.grid(row=0, column=1, sticky='E')
 
-    bill_length_mm = ttk.Entry(root,font=secondaryFontStyle,textvariable=content)
-    bill_length_mm.grid(row=1,column=1,sticky='E')
+    bill_length_mm = ttk.Entry(
+        root, font=secondaryFontStyle, textvariable=content, width=22)
+    bill_length_mm.grid(row=1, column=1, sticky='E')
 
-    bill_depth_mm = ttk.Entry(root,font=secondaryFontStyle)
-    bill_depth_mm.grid(row=2,column=1,sticky='E')
+    bill_depth_mm = ttk.Entry(root, font=secondaryFontStyle, width=22)
+    bill_depth_mm.grid(row=2, column=1, sticky='E')
 
-    flipper_length_mm = ttk.Entry(root,font=secondaryFontStyle)
-    flipper_length_mm.grid(row=3,column=1,sticky='E')
+    flipper_length_mm = ttk.Entry(root, font=secondaryFontStyle, width=22)
+    flipper_length_mm.grid(row=3, column=1, sticky='E')
 
-    body_mass_g = ttk.Entry(root,font=secondaryFontStyle)
-    body_mass_g.grid(row=4,column=1,sticky='E')
+    body_mass_g = ttk.Entry(root, font=secondaryFontStyle, width=22)
+    body_mass_g.grid(row=4, column=1, sticky='E')
 
-    sex = ttk.Combobox(root,font=secondaryFontStyle)
-    sex["values"] = ["Masculino","Femenino"]
-    sex.grid(row=5,column=1)
+    sex = ttk.Combobox(root, font=secondaryFontStyle)
+    sex["values"] = ["Masculino", "Femenino"]
+    sex.grid(row=5, column=1)
 
-    entradas = [island,bill_length_mm,bill_depth_mm,flipper_length_mm,body_mass_g,sex]
+    entradas = [island, bill_length_mm, bill_depth_mm,
+                flipper_length_mm, body_mass_g, sex]
 
-    tk.Label(root,text="mm",bg="white",font=fontStyle).grid(row=1,column=2,sticky='W')
-    tk.Label(root,text="mm",bg="white",font=fontStyle).grid(row=2,column=2,sticky='W')
-    tk.Label(root,text="mm",bg="white",font=fontStyle).grid(row=3,column=2,sticky='W')
-    tk.Label(root,text="g",bg="white",font=fontStyle).grid(row=4,column=2,sticky='W')
+    tk.Label(root, text="mm", bg="white", font=fontStyle).grid(
+        row=1, column=2, sticky='W')
+    tk.Label(root, text="mm", bg="white", font=fontStyle).grid(
+        row=2, column=2, sticky='W')
+    tk.Label(root, text="mm", bg="white", font=fontStyle).grid(
+        row=3, column=2, sticky='W')
+    tk.Label(root, text="g", bg="white", font=fontStyle).grid(
+        row=4, column=2, sticky='W')
 
     # ---------- SALIDAS
-    image = Image.open("p_default.png")
-    image = image.resize((225,225),Image.ANTIALIAS)
+    image = Image.open("./imgs/p_default.png")
+    image = image.resize((225, 225), Image.ANTIALIAS)
     imageTk = ImageTk.PhotoImage(image)
-    imageOutput = tk.Label(root,image=imageTk,bg="white",font=fontStyle)
-    imageOutput.grid(row=6,column=0,columnspan=2,rowspan=2,sticky="WENS",padx=5,pady=5)
-    
-    textOutput = tk.Label(root,text="No se ha determinado ninguna especie",bg="white",font=thirdFontStyle)
-    textOutput.grid(row=10,column=0,columnspan=3)
-    salidas = [imageOutput,textOutput]
+    imageOutput = tk.Label(root, image=imageTk, bg="white", font=fontStyle)
+    imageOutput.grid(row=6, column=0, columnspan=2,
+                     rowspan=2, sticky="WENS", padx=5, pady=5)
 
-    tk.Button(root,text="Especie",command=lambda:obtenerEspecie(entradas,salidas),bd=1,bg="#EB984E",relief="raised",width=20,font=thirdFontStyle).grid(row=9,column=0,columnspan=3)
+    textOutput = tk.Label(
+        root, text="No se ha determinado ninguna especie", bg="white", font=thirdFontStyle)
+    textOutput.grid(row=10, column=0, columnspan=3)
+    salidas = [imageOutput, textOutput]
+
+    tk.Button(root, text="Predecir Especie", command=lambda: obtenerEspecie(entradas, salidas), bd=1,
+              bg="#EB984E", relief="raised", width=20, font=thirdFontStyle).grid(row=9, column=0, columnspan=3)
 
     root.mainloop()
+
 
 if __name__ == "__main__":
     run()
--- mainGUI.py
+++ mainGUI.py
@@ -1,44 +1,171 @@
 import pandas as pd
 import numpy as np
-import tkinter as tk
 
-def printtext():
-    global e1
-    string = e1.get()
-    print(string)
+# --------------------------------TRAINED NEURAL NET DATA-------------------------------------
+
+trained_activations = [np.array([[-0.364402, -1.86106,  0.644491, -0.070859,  1.,  0.,
+                                  1.,  0.,  0.]]),
+                       np.array([[0.9875525, 0.88008342, 0.02839811, 0.99564306, 0.99592818,
+                                  0.21442287, 0.9937713, 0.98781454, 0.50631533]]),
+                       np.array([[1.56023348e-04, 3.81342998e-03, 9.96371288e-01]])]
+
+trained_weights = [np.array([[-3.37971049, -0.61816954, -0.16241541,  4.76223218,  4.63981161,
+                              -3.19517451,  4.32307045, -0.10763092,  0.83961083],
+                             [0.63560599,  0.2648269,  1.94940882, -2.619836, -2.70713842,
+                              1.80218476, -2.36600391, -1.88822734,  0.40891955],
+                             [0.45793873,  0.23139682, -1.6495256,  0.48928713,  0.40057207,
+                              0.34982515,  0.58040054,  1.84866072, -0.14027218],
+                             [2.04246248,  0.90160015, -1.62194568, -0.02228814,  0.07912638,
+                              0.36727169, -0.15364671,  1.73044273,  0.20291316],
+                             [1.19338278,  0.99679496,  0.77220602,  1.61168965,  1.32894253,
+                              -0.27248938,  1.08537906, -0.5553534,  0.79148492],
+                             [1.69760456,  0.70086895,  0.90418925, -0.26934511, -0.26322964,
+                              0.19349631, -0.5485062, -0.8835138,  0.42654247],
+                             [2.98124441,  1.17877522,  0.21212751,  0.36270433,  0.57069393,
+                              0.96423212,  0.77407126,  0.32845229,  0.40553981],
+                             [-0.57321083,  0.14239956,  1.07546772,  1.37482568,  1.58041382,
+                              -0.52783021,  1.64797556, -1.48997094,  1.14889076],
+                             [1.32225693,  0.80724207,  1.17617055, -0.79240847, -0.41698983,
+                              0.66076064, -0.11697339,  0.31778831,  0.68631277]]),
+                   np.array([[3.20800405, -5.94732906,  0.78753571],
+                             [0.47555428, -2.49113834, -0.41179534],
+                             [2.5003609,  1.81313074, -5.21773288],
+                             [-4.05916794,  3.17866956,  0.20001304],
+                             [-4.22035513,  2.86735828,  0.44374326],
+                             [3.19268485, -2.65035136, -1.20377782],
+                             [-3.30499473,  2.95508469,  0.80575194],
+                             [-1.40617493, -5.59718104,  4.88702026],
+                             [-0.37414805, -0.81193384, -1.30959131]])]
+
+def sigmoid(x):
+    y = 1.0 / (1 + np.exp(-x))
+    return y
+
+def sigmoid_derivative(x):
+    return x * (1.0 - x)
+
+def forward_propagate_input(X_input):
+    activations = X_input
+    trained_activations[0] = activations
+
+    # iterate through the network layers
+    for i, w in enumerate(trained_weights):
+        net_inputs = np.dot(activations, w)
+
+        activations = sigmoid(net_inputs)
+        # i+1: because activation = input of next neuron
+        trained_activations[i + 1] = activations
+
+    return activations
 
 
-root = tk.Tk()
-label = tk.Label(root, text="First Name")
-# tk.Label(root, text="Last Name").grid(row=1)
-label.pack()
+# ---------------------------------PRE-PROCESS INPUT------------------------------------
 
-b = tk.Button(root, text='okay', command=printtext)
-b.pack(side='bottom')
+# Convert to dictionary with labels so it can be sent to dataframe
+def first_process(input_list):
+    # input_list = ['Biscoe','42','13.5','210','4150','femenino']  # -> Es un Gento -> [0, 0, 1]
+    labels = ['island', 'bill_length_mm', 'bill_depth_mm',
+              'flipper_length_mm', 'body_mass_g', 'sex']
+    dict_data = dict()
+    for i, _ in enumerate(zip(input_list, labels)):
+        dict_data[labels[i]] = input_list[i]
 
-content = tk.StringVar()
-e1 = tk.Entry(root, textvariable=content)
-e1.pack()
-# e2 = tk.Entry(master)
-
-# e1.grid(row=0, column=1)
-# e2.grid(row=1, column=1)
-
-root.mainloop()
+    return dict_data
 
 
-# root = Tk()
+# Convert to dataframe from dictionary and rollback to original english labels
+def second_process(dict_data):
+    pd_data_oriented = pd.DataFrame.from_dict(dict_data, orient='index')
+    pd_data = pd_data_oriented.transpose()
+    pd_data['sex'].replace(['Femenino', 'Masculino'], [
+                           'female', 'male'], inplace=True)
 
-# root.title('Name')
-
-# e = Entry(root)
-# e.pack()
-# e.focus_set()
-
-# b = Button(root, text='okay', command=cliked)
-# b.pack(side='bottom')
-# root.mainloop()
+    return pd_data
 
 
-# # if __name__ == "__main__":
-# #     printtext()
+# Encode one hot
+def third_process(pd_data):
+    df_input = pd_data.copy()
+    encode = ['sex', 'island']
+
+    for col in encode:
+        dummy = pd.get_dummies(df_input[col], prefix=col)
+        df_input = pd.concat([df_input, dummy], axis=1)
+        del df_input[col]
+
+    return df_input
+
+
+# Add missing columns and fill with 0s
+def add_extra_cols(df):
+    labels = ['sex_female', 'sex_male', 'island_Biscoe',
+              'island_Dream', 'island_Torgersen']
+    dict_data = dict()
+    for label in labels:
+        if label not in df.columns:
+            dict_data[label] = 0
+            mock_df = pd.DataFrame.from_dict(
+                dict_data, orient='index').transpose()
+            new_df = pd.concat([df, mock_df], axis=1)
+
+    return new_df
+
+
+# Rearrange columns y normalize
+def rearrange_normalize(df_final):    
+    df_numbers = df_final.filter(['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g'])
+    df_genders = df_final.filter(['sex_female', 'sex_male'])
+    df_islands = df_final.filter(['island_Biscoe', 'island_Dream', 'island_Torgersen'])
+
+    # Normalize number data with mean and std from original dataset
+    means = [43.992793, 17.164865, 200.966967, 4207.057057]
+    stds = [5.468668, 1.969235, 14.015765, 805.215802]
+
+    # Manually normalize each value in row
+    for i, col in enumerate(list(df_numbers.columns)):
+        normal_data = float(df_numbers.at[0, col])
+        # normalize and round
+        normal_data = round((normal_data - means[i]) / stds[i], 6)
+        df_numbers.at[0, col] = str(normal_data)
+
+    # Rejoin separated columns to the right side in proper order (same as training input format)
+    formatted_df = pd.concat([df_numbers, df_genders], axis=1)
+    formatted_df = pd.concat([formatted_df, df_islands], axis=1)
+
+    return formatted_df
+
+
+# -------------------------------Process all input--------------------------------------
+def process_all_input(input_list):
+    first = first_process(input_list)    
+    second = second_process(first)    
+    third = third_process(second)    
+    df_final = add_extra_cols(third)
+    
+    arranged_df = rearrange_normalize(df_final)
+    
+    # Define test input in numpy array
+    X_input = arranged_df.to_numpy(dtype='float', copy=True)
+    
+    # Ask for the NN prediction
+    predicted_output = forward_propagate_input(X_input)
+    
+    # Aplicando redondeo a resultados de predicción:
+    rounded_predicted_output = np.around(predicted_output, 1).copy()
+        
+    # print(rounded_predicted_output)
+    print("Output is:")
+    print("Adelie = [1, 0, 0]", '||',
+          "Chinstrap = [0, 1, 0]", '||', "Gento = [0, 0, 1]")
+    
+    lst = rounded_predicted_output.tolist()
+    
+    for elem in lst:
+        if elem == [1.0, 0.0, 0.0]:
+            return 1
+        elif elem == [0.0, 1.0, 0.0]:
+            return 2
+        elif elem == [0.0, 0.0, 1.0]:
+            return 3
+        else:
+            return -1
